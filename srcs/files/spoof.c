fe3a504d8553c4252a2543ae76d19127dd2cc25e
tnoulens <tnoulens@42.fr>
2023-11-20 05:44:43 -0600

    reply mode

--- srcs/files/spoof.c
+++ srcs/files/spoof.c
@@ -11,6 +11,8 @@ void    poison(t_data *data, t_arp_packet *rep, struct ethhdr *eth)
 	ft_memcpy(eth->h_source, data->my_mac, ETH_ALEN);
 	// Edit Ethernet packet data
 	rep->arp_opcode = ntohs(ARPOP_REPLY);
+	// The addr in argv is in network order since the parsing
+	// Edit the destination and sender address and hardware address
 	rep->arp_spa = data->source.sin_addr.s_addr;
 	ft_memcpy(rep->arp_sha, data->source_mac, ETH_ALEN);
 	rep->arp_dpa = data->target.sin_addr.s_addr;
